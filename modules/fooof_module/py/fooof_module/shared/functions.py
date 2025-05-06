085a23687d4b407f96f7baca78e3836cdca34047
Nathan Kueh Tze Bing <nathankueh@Nathans-MacBook-Pro-10.local>
2025-05-05 23:33:55 -0400

    Cleaned up and added data description.

--- modules/fooof_module/py/fooof_module/shared/functions.py
+++ modules/fooof_module/py/fooof_module/shared/functions.py
@@ -15,46 +15,6 @@ from io import BytesIO
 
 
 ## DEFINE FUNCTIONS FOR DATA PROCESSING PIPELINE ##
-def extract_data(electrode, block, frequency, timestamp, length, plot=False):
-  """
-    Extract raw data for the selected electrodes.
-
-    Args:
-        electrode: the number of the electrode to be extracted.
-        block: the number of the experiment block to be extracted.
-        frequency: the sampling frequency of the data.
-        timestamp: the start time of the data to be extracted in seconds.
-        length: the length of the data to be extracted in seconds.
-    Returns:
-        voltage_data: the voltage data from the selected electrode and block.
-  """
-  electrode = str(electrode)
-  block = str(block)
-
-  # Ensure block is in righ form
-  if len(block) == 1:
-    block = '00' + block
-  elif len(block) == 2:
-    block = '0' + block
-
-  # Read data from electrode
-  electrode_data = h5py.File(FILEPATH + electrode + '.h5', 'r')
-
-  # Extract filtered data
-  voltage_data = electrode_data['ref']['voltage'][block]
-
-  # Extract the desired time split
-  if length is not None:
-    voltage_data = voltage_data[int(timestamp * frequency) : int((timestamp + length) * frequency)]
-
-  if plot:
-    plt.title(f"Electrode {electrode} block {block} voltage, time window: {timestamp} : {timestamp + length}s")
-    plt.plot(np.arange(0, len(voltage_data[int(timestamp * frequency):int((timestamp + length) * frequency)])/frequency, 1/frequency), voltage_data[int(timestamp * frequency) : int((timestamp + length) * frequency)])
-    plt.show()
-
-  return voltage_data
-
-
 def preprocess_data(data, frequency, freq_range, nperseg):
   """
     Transform data to frequency domain and filter out high frequencies.
@@ -81,6 +41,35 @@ def preprocess_data(data, frequency, freq_range, nperseg):
 
   return filtered_freqs, filtered_powers
 
+def preprocess_powers(subset_analyzed, fs=2000, nperseg=4000, freq_range=[1, 175]):
+    """
+    Processes each numeric column in the DataFrame using a spectral analysis and stores the results in new DataFrames.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the data.
+        fs (int): Sampling frequency of the data.
+        nperseg (int): Number of samples per segment.
+        freq_range (list): Frequency range as [min_freq, max_freq].
+
+    Returns:
+        pd.DataFrame, pd.DataFrame: Two DataFrames containing the power outputs and frequencies.
+    """
+    power_outputs = pd.DataFrame()
+    freq_in = False  # Flag to check if frequency column is added
+
+    # Loop through each column from the second column onward
+    for column in subset_analyzed.columns[1:]:  # Skip the first column
+        filtered_freqs, filtered_powers = preprocess_data(subset_analyzed[column].values, frequency=fs, freq_range=freq_range, nperseg=nperseg)
+        new_column_name = f"Trial_{column}_power"
+        
+        # Only add frequency data once
+        if not freq_in:
+            power_outputs["filtered_frequency"] = filtered_freqs
+            freq_in = True
+        
+        power_outputs[new_column_name] = filtered_powers
+
+    return power_outputs
 
 def generate_model_fit(filtered_freqs, filtered_powers, freq_range = [1,300], max_n_peaks = 10000,
                   aperiodic_mode = "fixed", plt_log = False, report = False):
@@ -146,47 +135,6 @@ def fit_fooof(filtered_freqs, filtered_powers, freq_range = [1, 175],
   }
   
   return fooof_fit
-  # fm.report(filtered_freqs_np, filtered_powers_np, freq_range)
-  # return fm
-  # 
-  # # Parameterize the power spectrum, and print out a plot
-  # # plot = fm.plot(plt_log = plt_log)
-  # plt.title(f"Condition Analyzed: {title}")
-  # plt.show()
-  
-  
-## Defining
-
-def get_electrode_data_during_experiment(experiment_info_df, electrodes, frequency, nperseg, freq_range, length):
-
-  for electrode in electrodes:
-    filt_freqs_list = []
-    filt_powers_list = []
-
-    for index, row in experiment_info_df.iterrows():
-      voltage_data = extract_data(electrode, str(row['Block']), frequency, row['Time'], length)
-      filtered_freqs, filtered_powers = preprocess_data(voltage_data, frequency, freq_range, nperseg)
-      filt_freqs_list.append(filtered_freqs)
-      filt_powers_list.append(filtered_powers)
-
-    experiment_info_df[f'filt_freqs electrode_{electrode}'] = filt_freqs_list
-    experiment_info_df[f'filt_powers electrode_{electrode}'] = filt_powers_list
-
-  return experiment_info_df
-
-
-# Dictionary: {"audio only": 1, "visual only": 2, "congruent": 3, "incongruent": 4}
-# Define function to assign condition types using regex
-def assign_condition_type(condition):
-    if re.search(r'_a$', condition):   # Ends with "_a"
-        return 1
-    elif re.search(r'_v$', condition):  # Ends with "_v"
-        return 2
-    elif re.search(r'_AV$', condition): # Ends with "_AV"
-        return 3
-    else:
-        return 4  # Everything else
-
 
 def add_mean_and_std(df, mean_col_name="Average Power", std_col_name="StdDev"):
     """
@@ -310,12 +258,6 @@ def plot_trials(df, conditions, Std_count=1, individual_trials=False):
   
     # Show the figure
     return fig
-    
-    
-def plot_raw_data(data, fs):
-  plt.plot(np.arange(0, len(data)/fs, 1/fs), data)
-  plt.show()
-
 
 def tune_max_n_peaks(df, freq_range, aperiodic_mode, peaks_range, conditions, show_errors=True):
   fig = go.Figure()
@@ -571,90 +513,6 @@ def tune_peak_threshold(df, freq_range, max_n_peaks, aperiodic_mode, start1, sto
         model_fail_message = f"Model fitting failed for condition(s): {', '.join(map(str, failed_conditions))}. Please adjust the peak threshold tuning parameter(s)."
     
     return {'plotly': fig, 'matplotlib': error_figs_base64, 'model_fail': model_fail_message}
-  
-
-def preprocess_powers(subset_analyzed, fs=2000, nperseg=4000, freq_range=[1, 175]):
-    """
-    Processes each numeric column in the DataFrame using a spectral analysis and stores the results in new DataFrames.
-
-    Args:
-        df (pd.DataFrame): The DataFrame containing the data.
-        fs (int): Sampling frequency of the data.
-        nperseg (int): Number of samples per segment.
-        freq_range (list): Frequency range as [min_freq, max_freq].
-
-    Returns:
-        pd.DataFrame, pd.DataFrame: Two DataFrames containing the power outputs and frequencies.
-    """
-    power_outputs = pd.DataFrame()
-    freq_in = False  # Flag to check if frequency column is added
-
-    # Loop through each column from the second column onward
-    for column in subset_analyzed.columns[1:]:  # Skip the first column
-        filtered_freqs, filtered_powers = preprocess_data(subset_analyzed[column].values, frequency=fs, freq_range=freq_range, nperseg=nperseg)
-        new_column_name = f"Trial_{column}_power"
-        
-        # Only add frequency data once
-        if not freq_in:
-            power_outputs["filtered_frequency"] = filtered_freqs
-            freq_in = True
-        
-        power_outputs[new_column_name] = filtered_powers
-
-    return power_outputs
-
-
-# def plot_fooof_fits(df, freq_range, max_n_peaks, aperiodic_mode, plt_log=False):
-#     # Create subplots: one for each condition
-#     num_conditions = len(df)
-#     fig = make_subplots(rows=num_conditions, cols=1, shared_xaxes=True, vertical_spacing=0.1, subplot_titles=[f'Condition {i+1}' for i in range(num_conditions)])
-# 
-#     for i in range(num_conditions):
-#         # Initialize storage
-#         dataframe = df[i]
-# 
-#         filtered_freqs_np = np.array(dataframe['filtered_frequency'])
-#         filtered_powers_np = np.array(dataframe['Average Power'])
-# 
-#         fm = SpectralModel(peak_width_limits=freq_range, max_n_peaks=max_n_peaks, aperiodic_mode=aperiodic_mode)
-#         fm.fit(filtered_freqs_np, filtered_powers_np, freq_range)
-# 
-#         # Get the model fit and add it as a trace
-#         model_freqs, model_fit, aperiodic_fit = fm.freqs, fm.get_model(), fm._ap_fit
-#         x_freqs = np.log10(model_freqs) if plt_log else model_freqs
-# 
-#         # Add traces to the corresponding subplot
-#         fig.add_trace(go.Scatter(x=x_freqs, y=model_fit,
-#                                  mode='lines',
-#                                  name=f'Full Model Fit - Condition {i+1}',
-#                                  line=dict(color='red')),
-#                       row=i+1, col=1)
-# 
-#         fig.add_trace(go.Scatter(x=x_freqs, y=aperiodic_fit,
-#                                  mode='lines',
-#                                  name=f'Aperiodic Fit - Condition {i+1}',
-#                                  line=dict(color='blue', dash='dash')),
-#                       row=i+1, col=1)
-# 
-#         # Add the original data as a baseline
-#         fig.add_trace(go.Scatter(x=x_freqs, y=fm.get_data(),
-#                                  mode='lines',
-#                                  name=f'Original Spectrum - Condition {i+1}',
-#                                  line=dict(color='black')),
-#                       row=i+1, col=1)
-# 
-#     # Customize the layout
-#     fig.update_layout(
-#         height=400 * num_conditions,  # Adjust the figure height based on number of conditions
-#         template='plotly_white',
-#         showlegend=True
-#     )
-#     for i in range(0, num_conditions):
-#       fig.update_xaxes(title_text='log(Frequency)' if plt_log else 'Frequency', row=i+1, col=1)
-#       fig.update_yaxes(title_text='Power', row=i+1, col=1)
-# 
-#     return fig
-
 
 def new_fit_fooof(df, conditions, freq_range = [1,300],
                max_n_peaks = 10000, aperiodic_mode = "fixed", plt_log = False):
@@ -756,15 +614,6 @@ def plot_fooof_fits(df, freq_range, max_n_peaks, aperiodic_mode, conditions, plt
                 textfont=dict(size=6),
                 showlegend=False
             ))
-      
-      # if condition_names:
-      #   name = condition_names[i]
-      #   # If name is a list, join the items
-      #   if isinstance(name, list):
-      #       label = ", ".join(name)
-      #   else:
-      #       label = name
-      #   title = f"FOOOF Model - Condition {i+1} ({label})"
         
       # Customize the layout
       fig.update_layout(
@@ -775,69 +624,3 @@ def plot_fooof_fits(df, freq_range, max_n_peaks, aperiodic_mode, conditions, plt
       )
       figures.append(fig)
   return figures
-
-
-# Tune aperiodic_mode
-# def tune_aperiodic_mode(df, freq_range, max_n_peaks, show_errors=True):
-#   fig = go.Figure()
-#   for i in range(0, len(df)):
-#       # Initialize storage
-#       dataframe = df[i]
-#       r2s = []
-#       errors = []
-#   
-#       # Initialize the Plotly figure
-#       # fig = go.Figure()
-#       
-#       filtered_freqs_np = np.array(dataframe['filtered_frequency'])
-#       filtered_powers_np = np.array(dataframe['Average Power'])
-#   
-#       # Define the aperiodic modes to loop over
-#       aperiodic_modes = ['fixed', 'knee']
-#   
-#       # Loop through aperiodic modes and fit models
-#       for aperiodic_mode in aperiodic_modes:
-#           nm = SpectralModel(peak_width_limits=freq_range, max_n_peaks=max_n_peaks, aperiodic_mode=aperiodic_mode)
-#           nm.fit(filtered_freqs_np, filtered_powers_np, freq_range)
-#   
-#           # Append metrics
-#           r2s.append(nm.r_squared_)
-#           errors.append(nm.error_)
-#   
-#           # Get the model fit and add it as a trace
-#           model_freqs, model_fit = nm.freqs, nm.get_model()
-#           fig.add_trace(go.Scatter(x=model_freqs, y=model_fit,
-#                                    mode='lines',
-#                                    name=f'{aperiodic_mode} (R²={nm.r_squared_:.2f}) - Condition {i+1}'))
-#   
-#       # Add the original data as a baseline (optional)
-#       fig.add_trace(go.Scatter(x=nm.freqs, y=nm.get_data(),
-#                                mode='lines', name=f'Original Spectrum - Condition {i+1}',
-#                                line=dict(color='black', dash='dash')))
-#     # Customize the layout
-#   fig.update_layout(
-#       title='Spectral Model Fits for Different Aperiodic Modes',
-#       xaxis_title='Frequency (Hz)',
-#       yaxis_title='Power',
-#       legend_title='Model Configurations',
-#       template='plotly_white'
-#   )
-#   return fig
-# 
-#   if show_errors:
-#       # Graphs of R2 and Errors using Matplotlib
-#       fig, axs = plt.subplots(1, 2, figsize=(16, 5))
-#       modes_labels = [f'{mode} mode' for mode in aperiodic_modes]
-#       
-#       axs[0].plot(modes_labels, r2s, "bo-")
-#       axs[0].set_title("R² vs. Aperiodic Mode")
-#       axs[0].set_ylabel("R²")
-#       axs[0].set_xlabel("Aperiodic Mode")
-# 
-#       axs[1].plot(modes_labels, errors, "ro-")
-#       axs[1].set_title("MSE vs. Aperiodic Mode")
-#       axs[1].set_ylabel("MSE")
-#       axs[1].set_xlabel("Aperiodic Mode")
-# 
-#       plt.tight_layout()
-#       plt.show()
